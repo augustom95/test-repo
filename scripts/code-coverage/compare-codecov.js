e2d8fbdbb82db449b152ab9c0ccf99208fe96e2f
Augusto Maneiro <augustom@nimble.la>
2022-01-24 14:22:32 -0300

    add if statements

--- scripts/code-coverage/compare-codecov.js
+++ scripts/code-coverage/compare-codecov.js
@@ -7,3 +7,16 @@ console.log('MASTER CODECOV', masterCodecov);
 
 const actualCodecov = readCoverageFromFile('coverage/coverage-summary.json');
 console.log('ACTUAL CODECOV', actualCodecov);
+
+if (actualCodecov < masterCodecov) {
+  console.error(`Actual code coverage has dropped ${masterCodecov - actualCodecov}%`);
+  process.exit(1);
+}
+
+if (actualCodecov > masterCodecov) {
+  console.log(`Actual code coverage increased ${actualCodecov - masterCodecov}%`);
+}
+
+if (actualCodecov = masterCodecov) {
+  console.log(`Actual code coverage stayed the same`);
+}
